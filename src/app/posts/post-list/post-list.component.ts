d37a7a344fe2fc200502ef1f4faa0f68369a63ba
shabeer85 <shab.ahmed085@gmail.com>
2018-08-06 22:51:20 +0530

    First Commit

---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from '@angular/core';
+
+@Component ({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+
+export class PostListComponent {
+  // posts = [
+  //   { title: 'First Post', content: 'This is first post\'s content' },
+  //   { title: 'Second Post', content: 'This is second post\'s content' },
+  //   { title: 'Third Post', content: 'This is third post\'s content' },
+  // ];
+
+  @Input() posts = [];
+}
