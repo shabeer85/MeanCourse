7eb214d90b6826e28553f1feb83584cbe965fa25
shabeer85 <shab.ahmed085@gmail.com>
2018-08-09 19:52:05 +0530

    Second push

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,4 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Post } from '../post.model';
+import { PostsService } from '../posts.service';
 
 @Component ({
   selector: 'app-post-list',
@@ -6,12 +9,27 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./post-list.component.css']
 })
 
-export class PostListComponent {
+export class PostListComponent implements OnInit, OnDestroy {
   // posts = [
   //   { title: 'First Post', content: 'This is first post\'s content' },
   //   { title: 'Second Post', content: 'This is second post\'s content' },
   //   { title: 'Third Post', content: 'This is third post\'s content' },
   // ];
 
-  @Input() posts = [];
+  posts: Post[] = [];
+  private postsSub: Subscription;
+
+  constructor(public postsService: PostsService) {}
+
+  ngOnInit() {
+    this.posts = this.postsService.getposts();
+    this.postsSub = this.postsService.getpostUpdatelistener().subscribe((posts: Post[]) => {
+      this.posts = posts;
+    });
+  }
+
+  ngOnDestroy() {
+    this.postsSub.unsubscribe();
+  }
+
 }
